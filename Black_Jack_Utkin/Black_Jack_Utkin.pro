68f87e65240409c2a4005be26a1a6a5d48a98572
Dmitry <utyan@mail.ru>
2020-07-25 17:35:58 +0300

    Creation of the project

---
+++ Black_Jack_Utkin/Black_Jack_Utkin.pro
@@ -0,0 +1,11 @@
+QT += core
+QT -= gui
+
+TARGET = Black_Jack_Utkin
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
---
+++ Black_Jack_Utkin/main.cpp
@@ -0,0 +1,15 @@
+    class Card{
+    protected:
+        enum VALUE {six, seven, eight, nine, ten, valet, queen, king, tyz};
+        enum SUIT {chervu, bubi, kresti, piki};
+        bool position;
+    public:
+        Card (){}
+        ~Card(){}
+        bool Flip(bool p){
+            position = -p;
+        }
+        bool GetValue(){
+            return VALUE;
+        }
+    };
