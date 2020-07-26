bc349a2cddb60e4df98c9ffaeb8079b1fee750ef
Dmitry <utyan@mail.ru>
2020-07-26 20:26:57 +0300

    Try pull request

--- Black_Jack_Utkin/main.cpp
+++ Black_Jack_Utkin/main.cpp
@@ -1,15 +1,61 @@
-    class Card{
-    protected:
-        enum VALUE {six, seven, eight, nine, ten, valet, queen, king, tyz};
-        enum SUIT {chervu, bubi, kresti, piki};
-        bool position;
-    public:
-        Card (){}
-        ~Card(){}
-        bool Flip(bool p){
-            position = -p;
+
+// BlackJack Game. v.1.3
+// by Dmitry Utkin
+// 25/07/2020
+// in QtCreator
+
+
+#include "iostream"
+#include <string>
+
+class Card{
+protected:
+    enum VALUE {two, three, four, five, six, seven,
+                eight, nine, ten, kadet, queen, king, tyz};
+    enum SUIT {chervu, bubi, kresti, piki};
+    VALUE CardValue[52];
+    SUIT CardSuit[4];
+    bool Position;
+    int Summ;
+public:
+    Card (){}
+    ~Card(){}
+    Card& Flip() {
+        if  (Position = false)
+            Position = true;
+        else
+            Position = false;
+        return Position;
+    }
+
+    int GetValue(){
+        if (VALUE = "tyz"){
+            return 1;
+        } else if (VALUE == "kadet" || VALUE == "queen" || VALUE == "king" || VALUE = ten){
+            return 10;
+        }else if (VALUE == "nine"){
+            return 9;
+        }else if (VALUE == "eight"){
+            return 8;
+        }else if (VALUE == "seven"){
+            return 7;
+        }else if (VALUE == "six"){
+            return 6;
+        }else if (VALUE == "five"){
+            return 5;
+        }else if (VALUE == "four"){
+            return 4;
+        }else if (VALUE == "three"){
+            return 3;
+        }else if (VALUE == "two"){
+            return 2;
         }
-        bool GetValue(){
-            return VALUE;// каваа банга!!!
-        }
-    };
+    }
+int main()
+
+{
+
+    return 0;
+}
+
+
